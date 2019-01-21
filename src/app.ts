cb9ec5fc730a8645155db85726e2f31e935f1418
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-01-20 22:34:04 -0500

    naieve collision detection

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import { Application } from 'pixi.js'
+import { Application, Graphics } from 'pixi.js'
 import Tile from './tile'
+
+const g = new Graphics()
+const tileBank = g.drawRect(0, 0, 200, 200)
+const tile = new Tile('/intersection.png')
+
+tile.setAllowedContainers([tileBank])
 
 const app = new Application(0, 0, { backgroundColor: 0x1099bb })
 
-app.stage.addChild(new Tile('/intersection.png').sprite)
+app.stage.addChild(tileBank)
+app.stage.addChild(tile.sprite)
 
 export default app
--- src/tile.ts
+++ src/tile.ts
@@ -1,9 +1,10 @@
-import { Texture, SCALE_MODES, Sprite, interaction } from 'pixi.js'
+import { Texture, SCALE_MODES, Sprite, interaction, Container } from 'pixi.js'
 
 class Tile {
   sprite: Sprite
   private interactionData: interaction.InteractionData | null = null
   private isDragging: boolean = false
+  private allowedContainers: Container[] = []
 
   constructor(imageUrl: string) {
     const texture = Texture.fromImage(imageUrl)
@@ -31,6 +32,22 @@ class Tile {
     this.sprite = sprite
   }
 
+  setAllowedContainers = (containers: Container[]) => {
+    this.allowedContainers = containers
+  }
+
+  isColliding = (el: Container): boolean => {
+    const { x: x1, y: y1, width: width1, height: height1 } = this.sprite
+    const { x: x2, y: y2, width: width2, height: height2 } = el
+
+    return (
+      x1 < x2 + width2 &&
+      x1 + width1 > x2 &&
+      y1 < y2 + height2 &&
+      y1 + height1 > y2
+    )
+  }
+
   private onDragStart = (e: interaction.InteractionEvent) => {
     this.interactionData = e.data
     this.sprite.alpha = 0.5
@@ -41,6 +58,11 @@ class Tile {
     this.sprite.alpha = 1
     this.isDragging = false
     this.interactionData = null
+    console.log('Checking collisions...')
+    this.allowedContainers.forEach((el, idx) => {
+      console.log(`Colliding with el ${idx}?:`, this.isColliding(el))
+    })
+    console.log('Collision check complete.')
   }
 
   private onDragMove = (_: interaction.InteractionEvent) => {
