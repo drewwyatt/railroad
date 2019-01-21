5d11653c745c7b5eb6f68685e92f8ad25d966869
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-01-21 00:21:28 -0500

    Formatting

--- src/tile-display.ts
+++ src/tile-display.ts
@@ -1,7 +1,10 @@
 import { DisplayObject, Graphics, Point } from 'pixi.js'
 
 enum RoadSide {
-  Top, Right, Bottom, Left
+  Top,
+  Right,
+  Bottom,
+  Left
 }
 
 enum RoadType {
@@ -48,8 +51,17 @@ class TileModel {
     roadWidth: number,
     arcRadius: number
   ) => {
-    const drawCarRoad = this.curriedDrawCarRoad(graphics, tileWidth, roadWidth, arcRadius)
-    const drawTrainRoad = this.curriedDrawTrainRoad(graphics, tileWidth, arcRadius)
+    const drawCarRoad = this.curriedDrawCarRoad(
+      graphics,
+      tileWidth,
+      roadWidth,
+      arcRadius
+    )
+    const drawTrainRoad = this.curriedDrawTrainRoad(
+      graphics,
+      tileWidth,
+      arcRadius
+    )
 
     if (this.top == RoadType.Car) {
       drawCarRoad(RoadSide.Top)
@@ -76,14 +88,18 @@ class TileModel {
     }
   }
 
-  private drawCenter = (graphics: Graphics, tileWidth: number, roadWidth: number) => {
+  private drawCenter = (
+    graphics: Graphics,
+    tileWidth: number,
+    roadWidth: number
+  ) => {
     if (this.blocked) {
       const padding = 4
       graphics.beginFill(0x000000)
       graphics.drawRect(
         tileWidth / 2 - roadWidth / 2 - padding,
-        tileWidth / 2 - roadWidth / 2 - padding, 
-        roadWidth + padding * 2, 
+        tileWidth / 2 - roadWidth / 2 - padding,
+        roadWidth + padding * 2,
         roadWidth + padding * 2
       )
       graphics.endFill()
@@ -95,27 +111,30 @@ class TileModel {
     tileWidth: number,
     roadWidth: number,
     arcRadius: number
-  ) => (
-    side: RoadSide
-  ) => {
-    const drawCarRoadSegment = this.curriedDrawCarRoadSegment(graphics, tileWidth, roadWidth, arcRadius)
+  ) => (side: RoadSide) => {
+    const drawCarRoadSegment = this.curriedDrawCarRoadSegment(
+      graphics,
+      tileWidth,
+      roadWidth,
+      arcRadius
+    )
     switch (side) {
-    case RoadSide.Top:
-      drawCarRoadSegment(this.left, false, false, RoadAxis.TopBottom)
-      drawCarRoadSegment(this.right, true, false, RoadAxis.TopBottom)
-      break
-    case RoadSide.Right:
-      drawCarRoadSegment(this.top, true, false, RoadAxis.LeftRight)
-      drawCarRoadSegment(this.bottom, true, true, RoadAxis.LeftRight)
-      break
-    case RoadSide.Bottom:
-      drawCarRoadSegment(this.top, true, true, RoadAxis.TopBottom)
-      drawCarRoadSegment(this.bottom, false, true, RoadAxis.TopBottom)
-      break
-    case RoadSide.Left:
-      drawCarRoadSegment(this.top, false, false, RoadAxis.LeftRight)
-      drawCarRoadSegment(this.bottom, false, true, RoadAxis.LeftRight)
-      break
+      case RoadSide.Top:
+        drawCarRoadSegment(this.left, false, false, RoadAxis.TopBottom)
+        drawCarRoadSegment(this.right, true, false, RoadAxis.TopBottom)
+        break
+      case RoadSide.Right:
+        drawCarRoadSegment(this.top, true, false, RoadAxis.LeftRight)
+        drawCarRoadSegment(this.bottom, true, true, RoadAxis.LeftRight)
+        break
+      case RoadSide.Bottom:
+        drawCarRoadSegment(this.top, true, true, RoadAxis.TopBottom)
+        drawCarRoadSegment(this.bottom, false, true, RoadAxis.TopBottom)
+        break
+      case RoadSide.Left:
+        drawCarRoadSegment(this.top, false, false, RoadAxis.LeftRight)
+        drawCarRoadSegment(this.bottom, false, true, RoadAxis.LeftRight)
+        break
     }
   }
 
@@ -135,11 +154,17 @@ class TileModel {
 
     const makePoint = this.curriedMakePoint(tileWidth, mirrorX, mirrorY, axis)
     const start = makePoint(0, midTile - midRoad)
-    
+
     graphics.moveTo(start.x, start.y)
     if (!this.blocked && adjacentRoadType == RoadType.Car) {
-      const arcStart = makePoint(midTile - midRoad - arcRadius, midTile - midRoad)
-      const arcCenter = makePoint(midTile - midRoad - arcRadius, midTile - midRoad - arcRadius)
+      const arcStart = makePoint(
+        midTile - midRoad - arcRadius,
+        midTile - midRoad
+      )
+      const arcCenter = makePoint(
+        midTile - midRoad - arcRadius,
+        midTile - midRoad - arcRadius
+      )
 
       graphics.lineTo(arcStart.x, arcStart.y)
       graphics.arc(
@@ -163,7 +188,10 @@ class TileModel {
   ) => (
     side: RoadSide // the road side to draw
   ) => {
-    const axis = (side == RoadSide.Left || side == RoadSide.Right) ? RoadAxis.LeftRight : RoadAxis.TopBottom
+    const axis =
+      side == RoadSide.Left || side == RoadSide.Right
+        ? RoadAxis.LeftRight
+        : RoadAxis.TopBottom
     const { curve, mirrorX, mirrorY } = this.curveAndMirrorTrain(side)
 
     const makePoint = this.curriedMakePoint(tileWidth, mirrorX, mirrorY, axis)
@@ -171,7 +199,10 @@ class TileModel {
     graphics.moveTo(start.x, start.y)
     if (curve) {
       const arcStart = makePoint(tileWidth / 2 - arcRadius, tileWidth / 2)
-      const arcCenter = makePoint(tileWidth / 2 - arcRadius, tileWidth / 2 - arcRadius)
+      const arcCenter = makePoint(
+        tileWidth / 2 - arcRadius,
+        tileWidth / 2 - arcRadius
+      )
 
       graphics.lineTo(arcStart.x, arcStart.y)
       graphics.arc(
@@ -188,17 +219,29 @@ class TileModel {
     }
   }
 
-  private curriedMakePoint = (tileWidth: number, mirrorX: boolean, mirrorY: boolean, axis: RoadAxis) => (x: number, y: number): Point => {
+  private curriedMakePoint = (
+    tileWidth: number,
+    mirrorX: boolean,
+    mirrorY: boolean,
+    axis: RoadAxis
+  ) => (x: number, y: number): Point => {
     const newX = axis == RoadAxis.LeftRight ? x : y
     const newY = axis == RoadAxis.LeftRight ? y : x
-    return new Point(mirrorX ? tileWidth - newX : newX, mirrorY ? tileWidth - newY : newY)
+    return new Point(
+      mirrorX ? tileWidth - newX : newX,
+      mirrorY ? tileWidth - newY : newY
+    )
   }
 
-  private startAngle = (mirrorX: boolean, mirrorY: boolean, axis: RoadAxis): number => {
+  private startAngle = (
+    mirrorX: boolean,
+    mirrorY: boolean,
+    axis: RoadAxis
+  ): number => {
     if (axis == RoadAxis.LeftRight && !mirrorY) {
       return Math.PI / 2
     } else if (axis == RoadAxis.LeftRight && mirrorY) {
-      return 3 * Math.PI / 2
+      return (3 * Math.PI) / 2
     } else if (axis == RoadAxis.TopBottom && !mirrorX) {
       return 0
     } else {
@@ -212,42 +255,88 @@ class TileModel {
       return Math.PI / 4
     } else if (!mirrorX && mirrorY) {
       // bottom left
-      return 7 * Math.PI / 4
+      return (7 * Math.PI) / 4
     } else if (mirrorX && !mirrorY) {
       // top right
-      return 3 * Math.PI / 4
+      return (3 * Math.PI) / 4
     } else {
       // bottom right
-      return 5 * Math.PI / 4
+      return (5 * Math.PI) / 4
     }
   }
 
-  private counterclockwise = (mirrorX: boolean, mirrorY: boolean, axis: RoadAxis): boolean => {
+  private counterclockwise = (
+    mirrorX: boolean,
+    mirrorY: boolean,
+    axis: RoadAxis
+  ): boolean => {
     if ((!mirrorX && !mirrorY) || (mirrorX && mirrorY)) {
-      return (axis == RoadAxis.LeftRight)
+      return axis == RoadAxis.LeftRight
     } else {
-      return (axis == RoadAxis.TopBottom)
+      return axis == RoadAxis.TopBottom
     }
   }
 
-  private curveAndMirrorTrain = (side: RoadSide): { curve: boolean, mirrorX: boolean, mirrorY: boolean } => {
+  private curveAndMirrorTrain = (
+    side: RoadSide
+  ): { curve: boolean; mirrorX: boolean; mirrorY: boolean } => {
     switch (side) {
-    case RoadSide.Top:
-      let curveRight = (this.right == RoadType.Train && this.left == RoadType.None && this.bottom == RoadType.None)
-      let curveLeft = (this.left == RoadType.Train && this.right == RoadType.None && this.bottom == RoadType.None)
-      return { curve: curveLeft || curveRight, mirrorX: curveRight, mirrorY: false }
-    case RoadSide.Right:
-      let curveTop = (this.top == RoadType.Train && this.bottom == RoadType.None && this.left == RoadType.None)
-      let curveBottom = (this.bottom == RoadType.Train && this.top == RoadType.None && this.left == RoadType.None)
-      return { curve: curveTop || curveBottom, mirrorX: true, mirrorY: curveBottom }
-    case RoadSide.Bottom:
-      curveRight = (this.right == RoadType.Train && this.left == RoadType.None && this.top == RoadType.None)
-      curveLeft = (this.left == RoadType.Train && this.right == RoadType.None && this.top == RoadType.None)
-      return { curve: curveLeft || curveRight, mirrorX: curveRight, mirrorY: true }
-    case RoadSide.Left:
-      curveTop = (this.top == RoadType.Train && this.bottom == RoadType.None && this.right == RoadType.None)
-      curveBottom = (this.bottom == RoadType.Train && this.top == RoadType.None && this.right == RoadType.None)
-      return { curve: curveTop || curveBottom, mirrorX: false, mirrorY: curveBottom }
+      case RoadSide.Top:
+        let curveRight =
+          this.right == RoadType.Train &&
+          this.left == RoadType.None &&
+          this.bottom == RoadType.None
+        let curveLeft =
+          this.left == RoadType.Train &&
+          this.right == RoadType.None &&
+          this.bottom == RoadType.None
+        return {
+          curve: curveLeft || curveRight,
+          mirrorX: curveRight,
+          mirrorY: false
+        }
+      case RoadSide.Right:
+        let curveTop =
+          this.top == RoadType.Train &&
+          this.bottom == RoadType.None &&
+          this.left == RoadType.None
+        let curveBottom =
+          this.bottom == RoadType.Train &&
+          this.top == RoadType.None &&
+          this.left == RoadType.None
+        return {
+          curve: curveTop || curveBottom,
+          mirrorX: true,
+          mirrorY: curveBottom
+        }
+      case RoadSide.Bottom:
+        curveRight =
+          this.right == RoadType.Train &&
+          this.left == RoadType.None &&
+          this.top == RoadType.None
+        curveLeft =
+          this.left == RoadType.Train &&
+          this.right == RoadType.None &&
+          this.top == RoadType.None
+        return {
+          curve: curveLeft || curveRight,
+          mirrorX: curveRight,
+          mirrorY: true
+        }
+      case RoadSide.Left:
+        curveTop =
+          this.top == RoadType.Train &&
+          this.bottom == RoadType.None &&
+          this.right == RoadType.None
+        curveBottom =
+          this.bottom == RoadType.Train &&
+          this.top == RoadType.None &&
+          this.right == RoadType.None
+        return {
+          curve: curveTop || curveBottom,
+          mirrorX: false,
+          mirrorY: curveBottom
+        }
     }
   }
 }
