518767b791f2436dd89c5e938a3afd2adad241de
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-01-20 23:29:53 -0500

    Now drawing all four road sides correctly

--- src/tile-display.ts
+++ src/tile-display.ts
@@ -15,7 +15,7 @@ class TileModel {
 
   constructor() {
     this.top = RoadType.Car
-    this.right = RoadType.None
+    this.right = RoadType.Car
     this.bottom = RoadType.Car
     this.left = RoadType.Car
     this.blocked = false
@@ -30,44 +30,43 @@ class TileModel {
     graphics.endFill()
 
     const arcRadius = 20
-    this.drawLeftCarRoad(graphics, tileWidth, roadWidth, arcRadius)
-    this.drawTopCarRoad(graphics, tileWidth, roadWidth, arcRadius)
+    this.drawCarRoadSegments(graphics, tileWidth, roadWidth, arcRadius)
 
-    // this.drawLeftRoad(graphics, tileWidth, roadWidth, 20)
-    // graphics.moveTo(0, midTile - midRoad)
-    // graphics.lineTo(midTile - midRoad - this.arcRadius, midTile - midRoad)
-    // graphics.arc(midTile - midRoad - this.arcRadius, midTile - midRoad - this.arcRadius, this.arcRadius, Math.PI / 2, 0, true)
-    // graphics.lineTo(midTile - midRoad, 0)
     return graphics
   }
 
   // MARK: - Car Roads
 
-  private drawLeftCarRoad = (
+  private drawCarRoadSegments = (
     graphics: Graphics,
     tileWidth: number,
     roadWidth: number,
     arcRadius: number
   ) => {
-    this.drawCarRoadSegment(graphics, tileWidth, roadWidth, arcRadius, this.top, false, false, RoadAxis.LeftRight)
-    this.drawCarRoadSegment(graphics, tileWidth, roadWidth, arcRadius, this.bottom, false, true, RoadAxis.LeftRight)
+    const drawCarRoadSegment = this.curriedDrawCarRoadSegment(graphics, tileWidth, roadWidth, arcRadius)
+    // left
+    drawCarRoadSegment(this.top, false, false, RoadAxis.LeftRight)
+    drawCarRoadSegment(this.bottom, false, true, RoadAxis.LeftRight)
+
+    // top
+    drawCarRoadSegment(this.left, false, false, RoadAxis.TopBottom)
+    drawCarRoadSegment(this.right, true, false, RoadAxis.TopBottom)
+
+    // right
+    drawCarRoadSegment(this.top, true, false, RoadAxis.LeftRight)
+    drawCarRoadSegment(this.bottom, true, true, RoadAxis.LeftRight)
+
+    // bottom
+    drawCarRoadSegment(this.top, true, true, RoadAxis.TopBottom)
+    drawCarRoadSegment(this.bottom, false, true, RoadAxis.TopBottom)
   }
 
-  private drawTopCarRoad = (
+  private curriedDrawCarRoadSegment = (
     graphics: Graphics,
     tileWidth: number,
     roadWidth: number,
     arcRadius: number
-  ) => {
-    this.drawCarRoadSegment(graphics, tileWidth, roadWidth, arcRadius, this.left, false, false, RoadAxis.TopBottom)
-    this.drawCarRoadSegment(graphics, tileWidth, roadWidth, arcRadius, this.right, true, false, RoadAxis.TopBottom)
-  }
-
-  private drawCarRoadSegment = (
-    graphics: Graphics,
-    tileWidth: number,
-    roadWidth: number,
-    arcRadius: number,
+  ) => (
     adjacentRoadType: RoadType,
     mirrorX: boolean, // whether the line is being drawn on the left or right side
     mirrorY: boolean, // whether the line is being drawn on the top or bottom side
@@ -76,7 +75,7 @@ class TileModel {
     const midTile = tileWidth / 2
     const midRoad = roadWidth / 2
 
-    const makePoint = this.makeCurriedPoint(tileWidth, mirrorX, mirrorY, axis)
+    const makePoint = this.curriedMakePoint(tileWidth, mirrorX, mirrorY, axis)
     const start = makePoint(0, midTile - midRoad)
     
     graphics.moveTo(start.x, start.y)
@@ -99,10 +98,10 @@ class TileModel {
     }
   }
 
-  private makeCurriedPoint = (tileWidth: number, mirrorX: boolean, mirrorY: boolean, axis: RoadAxis) => (x: number, y: number): Point => {
-    const newX = mirrorX ? tileWidth - x : x
-    const newY = mirrorY ? tileWidth - y : y
-    return axis == RoadAxis.LeftRight ? new Point(newX, newY) : new Point(newY, newX)
+  private curriedMakePoint = (tileWidth: number, mirrorX: boolean, mirrorY: boolean, axis: RoadAxis) => (x: number, y: number): Point => {
+    const newX = axis == RoadAxis.LeftRight ? x : y
+    const newY = axis == RoadAxis.LeftRight ? y : x
+    return new Point(mirrorX ? tileWidth - newX : newX, mirrorY ? tileWidth - newY : newY)
   }
 
   private startAngle = (mirrorX: boolean, mirrorY: boolean, axis: RoadAxis): number => {
